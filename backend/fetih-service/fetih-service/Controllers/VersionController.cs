e8f8ef5a2efff4ac6037745cb49ae40dff08a27e
Atakan Küpçüoğlu <55987231+kupcuogluatakan@users.noreply.github.com>
2022-05-14 00:01:03 +0300

    City and War method was added
    - CityDetail
    - WarResult

---
+++ backend/fetih-service/fetih-service/Controllers/VersionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fetih_service.Controllers
+{
+
+    //fetih db 
+    // Scaffold-DbContext -Connection "data source=fetih-live-db.mssql.somee.com;initial catalog=fetih-live-db;User Id=akupcuoglu_SQLLogin_3;Password=sd6onnksiw;MultipleActiveResultSets=True;App=EntityFramework" -Provider Microsoft.EntityFrameworkCore.SqlServer -OutputDir "Entities/FETIH" -Context "FetihDbContext" –Force
+
+    //      MS SQL Server address:	fetih-live-db.mssql.somee.com
+    //      Login name:	            akupcuoglu_SQLLogin_3
+    //      Login password:	        sd6onnksiw
+
+
+
+    [ApiController]
+    [Route("[controller]")]
+    public class VersionController : ControllerBase
+    {
+
+        private readonly ILogger<VersionController> _logger;
+
+        public VersionController(ILogger<VersionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "FETIH SERVICE", GetType().Assembly.GetName().Version.ToString(), "© 2022", DateTime.Now.AddHours(+3).ToString("dd/MM/yyyy HH:mm") };
+        }
+    }
+}
---
+++ backend/fetih-service/fetih-service/Services/WarService.cs
@@ -0,0 +1,46 @@
+﻿using fetih_service.Dtos;
+using fetih_service.Entities.FETIH;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace fetih_service.Services
+{
+    public interface IWarService
+    {
+        WarDto.Result WarResult(int allyId, int enemyId, int allyTroops, int enemyTroops);
+    }
+    public class WarService : IWarService
+    {
+        FetihDbContext _context;
+        public WarService(FetihDbContext context)
+        {
+            _context = context;
+        }
+
+        public WarDto.Result WarResult(int allyId, int enemyId, int allyTroops, int enemyTroops)
+        {
+            try
+            {
+                Random random = new Random();
+                //saldıran şehrin saldırı gücü 
+                int attack_power = _context.Attributes.Where(w => w.MetaDataId == allyId).Select(s => s.Value).FirstOrDefault();
+
+                //savunan şehrin savunma gücü
+                int defence_power = _context.Attributes.Where(w => w.MetaDataId == enemyId).Select(s => s.Value).FirstOrDefault();
+
+                //savaş katsayıları ile % kaç oranında kazanacağı belirlenir.
+                double trueProbability = ((Convert.ToDouble(allyTroops) * attack_power) / ((Convert.ToDouble(allyTroops) * attack_power) + (Convert.ToDouble(enemyTroops) * defence_power)));
+
+                // kazanma oranına göre sonuç üretilir.
+                bool result = random.NextDouble() < trueProbability;
+
+                return new WarDto.Result { IsSucceed = result };
+            }
+            catch
+            {
+                return new WarDto.Result { IsSucceed = false };
+            }
+        }
+    }
+}
