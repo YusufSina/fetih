c8bf51eacfc61a7b75c987473f12441f3c4678a2
Samet Yazıcı <sametyzc@hotmail.com>
2022-05-14 16:53:23 +0300

    contract functions have been added

--- interface/fetih/src/App.js
+++ interface/fetih/src/App.js
@@ -5,23 +5,28 @@ import Introduction from './components/Introduction';
 import ChainWarningModal from './components/Modals/ChainWarningModal';
 import Title from './components/Title';
 import { MetaMaskProvider, MetaMaskConsumer } from './context/MetaMaskContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <MetaMaskProvider>
-      <Title />
-      <MetaMaskConsumer>
-        {({ account, chainId }) => {
-          if (account === '') {
-            return <Introduction />;
-          }
-          if (chainId !== '0x4') {
-            return <ChainWarningModal />;
-          }
-          return <Game />;
-        }}
-      </MetaMaskConsumer>
-    </MetaMaskProvider>
+    <>
+      <MetaMaskProvider>
+        <Title />
+        <MetaMaskConsumer>
+          {({ account, chainId }) => {
+            if (account === '') {
+              return <Introduction />;
+            }
+            if (chainId !== '0x4') {
+              return <ChainWarningModal />;
+            }
+            return <Game />;
+          }}
+        </MetaMaskConsumer>
+      </MetaMaskProvider>
+      <ToastContainer />
+    </>
   );
 }
 
