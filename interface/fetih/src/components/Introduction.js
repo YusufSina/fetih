31fdaf33e7258a93a142fb131e05c5359373ff75
yusufsina <yusufsinayildiz@gmail.com>
2022-05-15 06:57:57 +0300

    update intro message

--- interface/fetih/src/components/Introduction.js
+++ interface/fetih/src/components/Introduction.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import { Container } from "react-bootstrap";
 
 function Introduction() {
   return (
     <>
       <img src="/assets/img/slider.png" className="w-100" alt="slider" />
-      <div
-        className="h-100 w-100 d-flex flex-row justify-content-center align-items-center"
-      >
+      <div className="h-100 w-100 d-flex flex-row justify-content-center align-items-center">
         <Container>
           <div className="d-flex flex-column justify-content-center align-items-center">
-
             <div className="row py-5">
               <div className="col-md-6">
                 <br />
@@ -19,16 +16,31 @@ function Introduction() {
                 <br />
                 <br />
                 <h2>Fetih Nedir?</h2>
-                <p className="fs-4">&emsp;Yeni topraklar fethetmeyi, şans ve savaş stratejinizi deneyimi yaşamak istiyorsanız, Fetih: aradığınız uzun soluklu oyun olabilir. Artık ülkenizi korumak ve geliştirmek tamamen sizin elinizde olacak. Soluksuz bir maceraya başlayacaksınız. Yeni topraklar fethederek, şehirlerin komutası siz olacaksınız!</p>
+                <p className="fs-4">
+                  Yeni topraklar fethetmeyi, şans ve savaş stratejinizi
+                  deneyimi yaşamak istiyorsanız, Fetih: aradığınız uzun soluklu
+                  oyun olabilir. Artık ülkenizi korumak ve geliştirmek tamamen
+                  sizin elinizde olacak. Soluksuz bir maceraya başlayacaksınız.
+                  Yeni topraklar fethederek, şehirlerin komutası siz
+                  olacaksınız!
+                </p>
               </div>
               <div className="col-md-6">
-                <img src="/assets/img/fighter1.png" className="mx-auto" alt="fighter1" />
+                <img
+                  src="/assets/img/fighter1.png"
+                  className="mx-auto"
+                  alt="fighter1"
+                />
               </div>
             </div>
 
             <div className="row py-5">
               <div className="col-md-6">
-                <img src="/assets/img/fighter2.png" className="mx-auto" alt="fighter2" />
+                <img
+                  src="/assets/img/fighter2.png"
+                  className="mx-auto"
+                  alt="fighter2"
+                />
               </div>
               <div className="col-md-6">
                 <br />
@@ -38,10 +50,16 @@ function Introduction() {
                 <br />
                 <br />
                 <br />
-                <h2>Fetih Nedir?</h2>
-                <p className="fs-4" style={{ textAlign: 'right' }}>
-                  Artık krallığınızı korumak ve geliştirmek tamamen sizin elinizde. Şehrin üç tane özelliği var;
-                  Saldırı Gücü, Savunma Gücü ve Asker Sayısı. Şehrin özelliklerine göre saldırı stratejisi önem kazanmaktadır. Asker sayısı oyundaki en önemli faktördür. Aktif olarak asker sayınızı artırırsanız oyunu kazanma ihtimaliniz oldukça artacaktır. Yeni topraklar fethedecek, kıyasıya geçen harplerde nasıl bir kumandan olduğunuzu kanıtlama fırsatına sahip olacaksınız.
+                <h2>Oyun Stratejisi</h2>
+                <p className="fs-4" style={{ textAlign: "right" }}>
+                  Artık krallığınızı korumak ve geliştirmek tamamen sizin
+                  elinizde. Şehrin üç tane özelliği var; Saldırı Gücü, Savunma
+                  Gücü ve Asker Sayısı. Şehrin özelliklerine göre saldırı
+                  stratejisi önem kazanmaktadır. Asker sayısı oyundaki en önemli
+                  faktördür. Aktif olarak asker sayınızı artırırsanız oyunu
+                  kazanma ihtimaliniz oldukça artacaktır. Yeni topraklar
+                  fethedecek, kıyasıya geçen harplerde nasıl bir kumandan
+                  olduğunuzu kanıtlama fırsatına sahip olacaksınız.
                 </p>
               </div>
             </div>
@@ -54,11 +72,18 @@ function Introduction() {
                 <br />
                 <br />
                 <br />
-                <h2>Fetih Nedir?</h2>
-                <p className="fs-4">&emsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <h2>Kazanç Modeli</h2>
+                <p className="fs-4">
+                  Bir haritadaki tüm bölgeleri fetheden kullanıcı oyun
+                  hazinesindeki tüm değerin %90’nını hakeder.{" "}
+                </p>
               </div>
               <div className="col-md-6">
-                <img src="/assets/img/fighter3.png" className="mx-auto" alt="fighter3" />
+                <img
+                  src="/assets/img/fighter3.png"
+                  className="mx-auto"
+                  alt="fighter3"
+                />
               </div>
             </div>
           </div>
